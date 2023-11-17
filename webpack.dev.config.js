8a473a43a68ba9c0fe14e183bd3138b84dea27f2
guzman jimenez luis alfonso <124893198+luisGuz12@users.noreply.github.com>
2023-11-17 01:46:41 +0000

    ⭐🎮creation and integration of babel u webpack The integration of babel and webpack was carried out to our project

---
+++ webpack.dev.config.js
@@ -0,0 +1,54 @@
+// Importar el modulo Path
+const path = require('path');
+
+// Exportamos un Configuration Options Object
+module.exports = {
+  // 1. Estableciendo el archivo indexador
+  // del front-end
+  entry: "./client/index.js",
+  // 2. Estableciendo el archivo de salida
+  output: {
+    // 2.1 Ruta Absoluta de Salida
+    path: path.resolve(__dirname, "public"),
+    // 2.2 Nombre del archivo de salida
+    filename: "bundle.js",
+    // 2.3 Ruta base de archivos estaticos
+    publicPath: "/"
+  },
+  // 3. Servidor de desarrollo
+  devServer: {
+    // 3.1 Folder de estaticos
+    static: path.join(__dirname, 'public'),
+    // 3.2 Puerto del servidor de desarrollo
+    port: 8080,
+    // 3.3 Definiendo el HOST
+    host: '0.0.0.0'
+  },
+  // Agregando un modulo a webpack
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    'modules': false,
+                    'useBuiltIns': 'usage',
+                    'targets': '> 0.25%, not dead',
+                    'corejs': 3
+                  }
+                ]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
