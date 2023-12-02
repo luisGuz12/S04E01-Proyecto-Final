1b22436fd7572bd5018320dfcf449bff7f3f7f0f
guzman jimenez luis alfonso <124893198+luisGuz12@users.noreply.github.com>
2023-12-02 20:29:23 +0000

    📖updated registration requirements and add the book

--- server/domains/books/book.model.js
+++ server/domains/books/book.model.js
@@ -5,12 +5,31 @@ const { Schema } = mongoose;
 
 // Construir un Schema
 const bookSchema = new Schema({
+  // titulo
   name: {
     type: String,
     // siempre se ocupa que no quieras un espacio vacio
     required: true,
   },
   description: {
+    type: String,
+    required: true,
+  },
+  autor: {
+    type: String,
+    // siempre se ocupa que no quieras un espacio vacio
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  isbn: {
+    type: String,
+    // siempre se ocupa que no quieras un espacio vacio
+    required: true,
+  },
+  numerocopias: {
     type: String,
     required: true,
   },
--- server/domains/books/book.validator.js
+++ server/domains/books/book.validator.js
@@ -1,26 +1,30 @@
-// Importando biblioteca de validacion
 import * as Yup from 'yup';
-
 import log from '../../config/winston';
 
-// Creando un esquema de validación para el proyecto
 const bookSchema = Yup.object().shape({
-  name: Yup.string().required('Se requiere un nombre del libro'),
+  name: Yup.string().required('Se requiere un titulo del libro'),
+  autor: Yup.string().required('Se requiere un autor'),
+  categoria: Yup.string().required('Se requiere una categoria del libro'),
+  numerocopias: Yup.number().required('Se requiere un numero de copias'), // Cambiado a tipo número
   description: Yup.string()
     .max(500, 'No escribir mas de 500 caracteres')
     .required('Se requiere una descripción del libro'),
+  isbn: Yup.string()
+    .max(13, 'No escribir mas de 13 caracteres')
+    .required('Se requiere un isbn del libro'),
 });
 
-// Creando el extractor de datos de la petición
 const getbook = (req) => {
-  // Extrayendo datos de la petición
-  const { name, description } = req.body;
+  const { name, autor, categoria, isbn, numerocopias, description } = req.body; // Se agregan todas las variables
   log.info(
-    `Se extraen datos de la petición: name ${name}, description: ${description}`,
+    `Se extraen datos de la petición: name ${name}, autor ${autor}, categoria ${categoria}, isbn ${isbn}, numero_copias ${numerocopias}, description: ${description}`,
   );
-  // Regresando el objeto proyecto
   return {
     name,
+    autor,
+    categoria,
+    isbn,
+    numerocopias,
     description,
   };
 };
